5a6f034c6a7f6f1e9adb0296e4028fe490126870
Rafael <rafaelsouzavalle@gmail.com>
2021-07-22 08:19:34 -0300

    Implemente React Router
    Implemente React Router para acionar os links do Nav menu.

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,14 +1,15 @@
 import './Nav.css'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default props =>
     <aside className="menu-area">
         <nav className="menu">
-            <a href="#/">
+            <Link to="/">
                 <i className="fa fa-folder"></i> Início
-            </a>
-            <a href="#/users">
+            </Link>
+            <Link to="/users">
                 <i className="fa fa-user-circle"></i> Usuários
-            </a>
+            </Link>
         </nav>
     </aside>
